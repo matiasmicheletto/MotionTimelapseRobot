c4cec1d8360f20a08350c34ae3bb5e2a67b45bc9
Matias <matiasmicheletto@gmail.com>
2020-08-05 20:12:23 -0300

    Fix calculos

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,9 +1,10 @@
 #include "BYJ48.h"
 
-#define SPT 8192                                // Pasos por vuelta (half-stepping)
-#define DPT 11.3                                // Distancia recorrida por vuelta (cm)
-#define DX 100                                  // Distancia a recorrer (cm)
-#define TD 3600                                 // Tiempo de desplazamiento (segs)
+// Van en formato float por cuestiones de casting
+#define SPT 8192.00                             // Pasos por vuelta (half-stepping)
+#define DPT 11.30                               // Distancia recorrida por vuelta (cm)
+#define DX 100.00                               // Distancia a recorrer (cm)
+#define TD 3600.00                              // Tiempo de desplazamiento (segs)
 
 BYJ48 motor(8, 9, 10, 11, true);                // Pines de coneccion y true por modo half-steping
 
